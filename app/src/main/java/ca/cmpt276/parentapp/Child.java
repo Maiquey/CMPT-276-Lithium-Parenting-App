1f82d5d22ede43663ad923400cf3ae0c69d3eb76
Hitesh Juneja <hja54@sfu.ca>
2021-11-01 11:33:36 -0700

    Added Child and ChildManager

---
+++ app/src/main/java/ca/cmpt276/parentapp/Child.java
@@ -0,0 +1,18 @@
+package ca.cmpt276.parentapp;
+
+public class Child {
+    private String name;
+
+    public Child(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
