d809a8c1d4df3243967671ec67cc3b07bb439166
janitkumar <janitk@sfu.ca>
2021-11-01 18:03:28 -0700

    Implement main menu screen.

--- app/src/main/java/ca/cmpt276/parentapp/MainActivity.java
+++ app/src/main/java/ca/cmpt276/parentapp/MainActivity.java
@@ -78,7 +78,6 @@ public class MainActivity extends AppCompatActivity {
 //        if (id == R.id.action_settings) {
 //            return true;
 //        }
-
         return super.onOptionsItemSelected(item);
     }
 
