0cd440722eb3e82d75bd3aa65e62b8f07b8e06e4
Hitesh Juneja <hja54@sfu.ca>
2021-11-27 00:50:10 -0800

    Create TakeBreath Activity and Add Inhale & Exhale Sound

---
+++ app/src/main/java/ca/cmpt276/parentapp/ui/TakeBreath.java
@@ -0,0 +1,16 @@
+package ca.cmpt276.parentapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.cmpt276.parentapp.R;
+
+public class TakeBreath extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_breath);
+    }
+}
